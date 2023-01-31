247d3fa8762b59af0031d9be3cddf16804adee57
PankajGosavi1608 <pankajudaygosawi@gmail.com>
2023-01-31 21:14:00 +0530

    Project Add

---
+++ src/main/java/com/BikkadIT06/PankajGosavi.java
@@ -0,0 +1,18 @@
+package com.BikkadIT06;
+
+public class PankajGosavi {
+	
+	
+	public void PankajGosavi()
+	{
+		System.out.println("Method Created By Pankaj Gosavi");
+	}
+
+	public static void main(String[] args) {
+		
+		PankajGosavi PG=new PankajGosavi();
+		PG.PankajGosavi();
+
+	}
+
+}
---
+++ src/test/java/com/BikkadIT06/BikkadIt06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikkadIt06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
