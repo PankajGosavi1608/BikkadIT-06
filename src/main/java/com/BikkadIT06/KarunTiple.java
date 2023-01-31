17bbdb561f2b1842385d153f44ebf3330ac3bc9e
karuntiple <tiplekarun@gmail.com>
2023-01-31 22:27:49 +0530

    Changes by Dev-KarunTiple

---
+++ src/main/java/com/BikkadIT06/KarunTiple.java
@@ -0,0 +1,14 @@
+package com.BikkadIT06;
+
+public class KarunTiple {
+
+	public void karunTiple() {
+		System.out.println("Method Created By Karun Tiple");
+	}
+
+	public static void main(String[] args) {
+
+		KarunTiple kt = new KarunTiple();
+		kt.karunTiple();
+	}
+}
